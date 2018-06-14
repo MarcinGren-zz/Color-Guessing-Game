34dabdf0ec541f3c9220f7ac0196baa1923e321b
Marcin Gren <marcin.gren@gmail.com>
2018-06-14 19:44:20 +0200

    new colors button added

--- colorGame.js
+++ colorGame.js
@@ -1,8 +1,10 @@
 var testColor = document.querySelector(".col-sm-4")
 var allColorSquares = document.querySelectorAll(".thumbnail")
+var rgbHtmlAnswer = document.querySelector("#rgbanswer")
+var newColorsButton = document.querySelector("#newcolors")
 
-var randomAnswer = Math.floor(Math.random() * 6)
-var correctAnswer = getRGBFromRandomedAnswer()
+var correctAnswer = "" //these 3 are temp, might refactor them later
+var randomAnswer = Math.floor(Math.random() * 6) //these 3 are temp, might refactor them later
 
 function randomNumberRGB() {
     return Math.floor(Math.random() * 255)
@@ -13,13 +15,15 @@ function randomColor() {
 }
 
 function getRGBFromRandomedAnswer() { //can move randomAnswer inside
-    return allColorSquares[randomAnswer].style.background.toUpperCase()
+    correctAnswer = allColorSquares[randomAnswer].style.background.toUpperCase()
+    rgbHtmlAnswer.innerHTML = correctAnswer //these 3 are temp, might refactor them later
 }
 
 function randomAllBoxesColors() {
     for (var i = 0; i < allColorSquares.length; i++) {
         allColorSquares[i].style.background = randomColor()
     }
+    getRGBFromRandomedAnswer()
 }
 
 // //so this is working and randoming for the 1 element and its also a border
@@ -27,3 +31,4 @@ function randomAllBoxesColors() {
 //     testColor.style.border = "3px solid " + randomColor()
 // })
 randomAllBoxesColors()
+newColorsButton.addEventListener("click", randomAllBoxesColors)
